c8f70981ff418d4104ffe0d878fed55e3c535c11
ezequiel magi <ezequielmaggi@gmail.com>
2022-02-19 14:58:59 -0300

    creando lista de categorias

--- proyect-giphy/src/components/GifExpertApp.js
+++ proyect-giphy/src/components/GifExpertApp.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+  // const categories = ['One Punch' , 'Samuray X' , 'Dragon Ball']
+
+  const [categories, setCategories] = useState(['One Punch' , 'Samuray X' , 'Dragon Ball'])
+
+  const handleAdd = () => {
+    setCategories( [...categories , 'X Men'] )
+  }
+
   return (
 <>
       <h2>GiftExpertApp</h2>
-    <hr />
+      <hr /> 
+
+      <button onClick={handleAdd}>ADD</button>
+
+      <ol>
+        {
+          categories.map( category=> {
+            return <li key={category} > { category } </li>
+          }) 
+        }
+      </ol>
+      
 </>
   )
 }
